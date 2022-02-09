cb5c20c4ce3e759170ffc65045ef8d1c898437ff
leeuihyun <meta212121@naver.com>
2022-02-09 13:33:45 +0900

    react

--- first/src/IterationSample.js
+++ first/src/IterationSample.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 const IterationSample = () => {
-  const names = ["눈", "사람", "얼음", "겨울"];
-  const arr = names.map((item, idx) => <li key={idx}>{item}</li>);
-  return <ul>{arr}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "겨울" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [next_id, setId] = useState(names.length);
+  const [next_text, setText] = useState("");
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const onClick = (e) => {
+    setId(next_id + 1);
+    const next = names.concat({
+      id: next_id,
+      text: next_text,
+    });
+    setNames(next);
+    setText("");
+  };
+  const list = names.map((item) => <li key={item.id}>{item.text}</li>);
+  return (
+    <div>
+      <input type="text" onChange={onChange} />
+      <button onClick={onClick}>button</button>
+      <ul>{list}</ul>
+    </div>
+  );
 };
 
 export default IterationSample;
