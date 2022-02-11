fc1355ab18c537ee62e0edc5956f89906164b3ed
leeuihyun <meta212121@naver.com>
2022-02-11 14:33:38 +0900

    CSSModule.module.css 사용법 (CSSModule)

--- third/src/App.js
+++ third/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
 function App() {
   return (
     <div>
-      <SassComponent />
+      <CSSModule></CSSModule>
     </div>
   );
 }
