d471a02f6cdd3c5582399970c8a7656b1413aeae
leeuihyun <meta212121@naver.com>
2022-02-16 15:53:11 +0900

    categories

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import NewsList from './components/NewsList';
-
+import Categories from './components/Categories';
 function App() {
   return (
     <div>
+      <Categories />
       <NewsList />
     </div>
   );
