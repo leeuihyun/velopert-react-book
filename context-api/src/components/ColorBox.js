a430a7c81bcc2ab086aee7685c789dcc5bc95212
leeuihyun <meta212121@naver.com>
2022-02-16 19:11:45 +0900

    context api Consumer,Provider 개념 이해

--- context-api/src/components/ColorBox.js
+++ context-api/src/components/ColorBox.js
@@ -1,19 +1,29 @@
 import React from "react";
 import ColorContext from "../contexts/color";
+import { ColorConsumer } from "../contexts/color";
 
 function ColorBox() {
   return (
-    <ColorContext.Consumer>
+    <ColorConsumer>
       {(value) => (
-        <div
-          style={{
-            width: "64px",
-            height: "64px",
-            background: value.color,
-          }}
-        ></div>
+        <>
+          <div
+            style={{
+              width: "64px",
+              height: "64px",
+              background: value.state.color,
+            }}
+          ></div>
+          <div
+            style={{
+              width: "24px",
+              height: "24px",
+              background: value.state.subcolor,
+            }}
+          ></div>
+        </>
       )}
-    </ColorContext.Consumer>
+    </ColorConsumer>
   );
 }
 
