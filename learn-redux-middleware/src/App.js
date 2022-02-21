005bbd16142664359b1c7e7eeb8ac58685d26b7c
leeuihyun <meta212121@naver.com>
2022-02-21 17:14:56 +0900

    ✨React middleware

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import CounterContainer from "./containers/CounterContainer";
+function App() {
+  return <CounterContainer></CounterContainer>;
+}
+
+export default App;
