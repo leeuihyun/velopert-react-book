dba61a236401eab310d5709b9f71c43c0443b3c2
leeuihyun <meta212121@naver.com>
2022-02-21 15:26:26 +0900

    ✨react-redux 의 useSelector() 이용하기

--- practice/src/containers/CounterContainer.js
+++ practice/src/containers/CounterContainer.js
@@ -1,6 +1,9 @@
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useCallback } from "react";
+/*
 function CounterContainer({ increase, decrease, number }) {
   return (
     <Counter
@@ -20,3 +23,20 @@ export default connect(
     decrease,
   }
 )(CounterContainer);
+*/
+
+function CounterContainer() {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  return (
+    <Counter
+      number={number}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+    ></Counter>
+  );
+}
+
+export default CounterContainer;
