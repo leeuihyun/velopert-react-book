e3ff5f61884c5a125891e3bffc362c33fd41fd61
leeuihyun <meta212121@naver.com>
2022-02-23 17:48:28 +0900

    ⭕ Node.js blog Backend

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,15 +1,25 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
+const api = require('./api');
 
 router.get('/', (ctx) => {
   ctx.body = '홈';
 });
-router.get('/about', (ctx) => {
-  ctx.body = '소개';
+
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name}의 소개` : '소개';
 });
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `post #${id}` : 'no post ID';
+});
+router.use('/api', api.routes());
+app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
 //app 인스턴스에 라우터 적용
