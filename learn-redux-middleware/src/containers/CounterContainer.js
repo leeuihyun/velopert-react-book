0f36648d33b80e548fd43363e544fc3689682bca
UihyunLee <88185154+leeuihyun@users.noreply.github.com>
2022-02-24 01:56:43 +0900

    React (#1)
    * use ReduxThunk
    
    * ⚜ React ReduxThunk
    
    * blog-backend
    
    * ⭕ Node.js blog Backend
    
    * backend
    
    * ✨Blog Frontend
    
    * ✨ react-router-dom, component setting

--- learn-redux-middleware/src/containers/CounterContainer.js
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -2,12 +2,13 @@ import React, { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
+import { increaseAsync, decreaseAsync } from "../modules/counter";
 
 function CounterContainer() {
   const number = useSelector((state) => state.counter.number);
   const dispatch = useDispatch();
-  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
-  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  const onIncrease = useCallback(() => dispatch(increaseAsync()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decreaseAsync()), [dispatch]);
   return (
     <Counter
       number={number}
