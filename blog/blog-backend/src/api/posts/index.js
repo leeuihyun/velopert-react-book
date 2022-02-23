2acb3f5aebb5379d0c302d1ba4bf4fb6ba097bb8
leeuihyun <meta212121@naver.com>
2022-02-23 18:26:40 +0900

    backend

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,12 +1,17 @@
-const Router = require('koa-router');
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
+
 const posts = new Router();
-const postsCtrl = require('./posts.ctrl');
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
-module.exports = posts;
+const post = new Router(); // /api/posts/:id
+post.get('/', postsCtrl.read);
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
+
+posts.use('/:id', postsCtrl.getPostById, post.routes());
+
+export default posts;
--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,28 +1,11 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+import Router from 'koa-router';
+import posts from './posts';
+import auth from './auth';
 
-const app = new Koa();
-const router = new Router();
-const api = require('./api');
+const api = new Router();
 
-router.get('/', (ctx) => {
-  ctx.body = '홈';
-});
+api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = name ? `${name}의 소개` : '소개';
-});
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  ctx.body = id ? `post #${id}` : 'no post ID';
-});
-router.use('/api', api.routes());
-app.use(bodyParser());
-app.use(router.routes()).use(router.allowedMethods());
-
-//app 인스턴스에 라우터 적용
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
-});
+// 라우터를 내보냅니다.
+export default api;
