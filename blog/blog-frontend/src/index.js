d6ca63f917b1fe4f522f0280478e6017c073be15
leeuihyun <meta212121@naver.com>
2022-02-24 17:15:04 +0900

    ⚡REACTERS Project Ui setting

--- blog/blog-frontend/src/index.js
+++ blog/blog-frontend/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import rootReducer from './modules';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
