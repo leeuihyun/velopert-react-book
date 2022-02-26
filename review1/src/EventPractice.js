369f3490ff691b7fee98004a176ca9b5e71543df
leeuihyun <meta212121@naver.com>
2022-02-27 00:19:13 +0900

    useState review

--- review1/src/EventPractice.js
+++ review1/src/EventPractice.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import {useState} from 'react';
 
 function EventPractice() {
-  const onChange =(e) => {
-    console.log(e);
-  }
+  const [user,setUser] = useState({
+      name : '',
+      id : ''
+  })
+
+    const onChange =(e) => {
+        const {value,name} = e.target;
+        console.log(value);
+        setUser({
+            ...user,
+            [name] : value
+        })
+     }
+     const onClick = ()=>{
+        const {name,id} = user;
+        alert(name + ':' + id);
+     }
     return (
     <div>
         <h1>Event practice</h1>
-        <input type="text" placeholder = "무엇이든 입력해봐!" onChange = {onChange}/>
+        <input type="text" placeholder = "name" onChange = {onChange} name = "name" value = {user.name}/>
+        <input type="text" placeholder = "id" onChange = {onChange} name = "id" value = {user.id}/>
+        <button onClick = {onClick}>button</button>
     </div>
   )
 }
