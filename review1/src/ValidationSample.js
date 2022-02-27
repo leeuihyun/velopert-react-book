e1a2655dd533104c90188ee0ac18ab3300292007
leeuihyun <meta212121@naver.com>
2022-02-27 23:27:28 +0900

    React review ValidationSample useRef

---
+++ review1/src/ValidationSample.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react';
+import { useRef } from 'react';
+function ValidationSample() {
+    const inputRef = useRef(null);
+    const onChange = (e) => {
+        console.log(e.target.value);
+    }
+    const onClick = e => {
+        inputRef.current.focus();
+    }
+    return (
+        <div>
+            <input type="text" onChange = {onChange} ref = {inputRef}/>
+            <button onClick = {onClick}>button</button>
+        </div>
+  )
+}
+
+export default ValidationSample;
